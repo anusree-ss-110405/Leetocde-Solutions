f4f7e8acf1fd1450ced135e085f1f62e3ca51989
ANUSREE S S <sreeanu110405@gmail.com>
2024-03-07 19:57:02 +0530

    Commit

---
+++ Leetcode_1587.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select name, sum(amount) as balance
+from Users
+inner join Transactions on Users.account=Transactions.account
+group by Users.account
+having sum(amount) > 10000
